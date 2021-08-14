380787429f466a167be9fffe5bffdc23469d9ded
shouravrahman <shouravrahman006@gmail.com>
2021-08-15 01:13:12 +0600

    category api ok

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,10 +1,21 @@
 //model
 const Category = require('../models/category');
-
+//imports
+const slugify = require('slugify');
 //functions for each type of request
 
-exports.create = (req, res) => {};
-exports.list = (req, res) => {};
-exports.read = (req, res) => {};
-exports.update = (req, res) => {};
-exports.remove = (req, res) => {};
+exports.create = async (req, res) => {
+	try {
+		//to create a category first we need to grab the name from the req body coming from frontend
+		const { name } = req.body;
+		//then we can create a new category using the name ,Category model and generate a slug on the fly
+		const category = await new Category({ name, slug: slugify(name) }).save();
+		res.json(category);
+	} catch (err) {
+		res.status(400).send('create category failed');
+	}
+};
+exports.list = async (req, res) => {};
+exports.read = async (req, res) => {};
+exports.update = async (req, res) => {};
+exports.remove = async (req, res) => {};
