c95f517b7dfe363f2ca147726d283274c8d31446
shouravrahman <shouravrahman006@gmail.com>
2021-08-17 00:06:02 +0600

    updating functionality done

---
+++ client/src/pages/admin/category/UpdateCategory.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { getCategory, updateCategory } from '../../../functions/category';
+import AdminNav from '../../../components/nav/AdminNav';
+import CategoryForm from '../../../components/forms/CategoryForm';
+
+const UpdateCategory = ({ history, match }) => {
+	const [name, setName] = useState('');
+	const [loading, setLoading] = useState(false);
+	const { user } = useSelector((state) => ({ ...state }));
+
+	const loadCategory = () =>
+		getCategory(match.params.slug).then((categories) =>
+			setName(categories.data.name)
+		);
+
+	useEffect(() => {
+		loadCategory();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		updateCategory(match.params.slug, { name }, user.token)
+			.then((res) => {
+				setLoading(false);
+				setName('');
+				toast.success(`'${res.data.name} category updated`);
+				history.push('/admin/category');
+			})
+			.catch((err) => {
+				setLoading(false);
+				if (err.response.status === 400) toast.error(err.response.data);
+			});
+	};
+
+	return (
+		<div className='container-fluid'>
+			<div className='row'>
+				<div className='colmd-2'>
+					<AdminNav />
+				</div>
+				<div className='col'>
+					{loading ? (
+						<h5 className='text-danger'>loading..</h5>
+					) : (
+						<h4>Update category</h4>
+					)}
+					<CategoryForm
+						handleSubmit={handleSubmit}
+						name={name}
+						setName={setName}
+					/>
+					<hr />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default UpdateCategory;
