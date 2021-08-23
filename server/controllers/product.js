8fb3057cf5185e2355da389fb2c6276c0ca838f5
shouravrahman <shouravrahman006@gmail.com>
2021-08-24 00:35:16 +0600

    product remove function

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,23 +1,40 @@
-const Product = require('../models/product');
-const slugify = require('slugify');
-const { find } = require('../models/user');
+const Product = require('../models/product')
+const slugify = require('slugify')
+const { find } = require('../models/user')
 
 exports.create = async (req, res) => {
 	try {
-		console.log(req.body);
-		req.body.slug = slugify(req.body.title);
-		const newProduct = await new Product(req.body).save();
-		res.json(newProduct);
+		console.log(req.body)
+		req.body.slug = slugify(req.body.title)
+		const newProduct = await new Product(req.body).save()
+		res.json(newProduct)
 	} catch (err) {
-		console.log(err);
+		console.log(err)
 		// res.status(400).send('create category failed');
 		res.status(400).json({
 			err: err.message,
-		});
+		})
 	}
-};
+}
 
-exports.read = async (req, res) => {
-	let products = await Product.find({});
-	res.json(products);
-};
+exports.listAll = async (req, res) => {
+	let products = await Product.find({})
+		.limit(parseInt(req.params.count))
+		.populate('category')
+		.populate('subcategory')
+		.sort([['createdAt', 'desc']])
+		.exec()
+	res.json(products)
+}
+
+exports.remove = async (req, res) => {
+	try {
+		const deleted = await Product.findOneAndRemove({
+			slug: req.params.slug,
+		}).exec()
+		res.json(deleted)
+	} catch (err) {
+		console.log(err)
+		return res.status(400).send('Product delete failed')
+	}
+}
