1605d9d3dc23c6d0bb701c2aa2545b6d0f44e463
shouravrahman <shouravrahman006@gmail.com>
2021-08-24 00:35:05 +0600

    product remove route

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 //controllers
-const { create, read } = require('../controllers/product');
+const { create, listAll, remove } = require('../controllers/product')
 
 //middlewares
-const { authCheck, adminCheck } = require('../middlewares/auth');
+const { authCheck, adminCheck } = require('../middlewares/auth')
 
 //routes
-router.post('/create', authCheck, adminCheck, create);
-router.get('/products', read);
+router.post('/create', authCheck, adminCheck, create)
+router.get('/products/:count', listAll)
+router.delete('/product/:slug', authCheck, adminCheck, remove)
 
-module.exports = router;
+module.exports = router
