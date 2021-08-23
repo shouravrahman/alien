9f71a0fd9df5cba1c445390f9b997077d25518f6
shouravrahman <shouravrahman006@gmail.com>
2021-08-24 00:34:04 +0600

    list product route

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Header from './components/nav/Header';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Home from './pages/Home';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import RegisterComplete from './pages/auth/RegisterComplete';
-import { auth } from './firebase';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import { currentUser } from './functions/auth';
-import History from './pages/user/History';
-import UserRoute from './components/routes/UserRoute';
-import AdminRoute from './components/routes/AdminRoute';
-import Password from './pages/user/Password';
-import Wishlist from './pages/user/Wishlist';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import CreateCategory from './pages/admin/category/CreateCategory';
-import UpdateCategory from './pages/admin/category/UpdateCategory';
-import CreateSubcategory from './pages/admin/subcategory/CreateSubcategory';
-import UpdateSubcategory from './pages/admin/subcategory/UpdateSubcategory';
-import CreateProduct from './pages/admin/product/CreateProduct';
+import './App.css'
+import { Switch, Route } from 'react-router-dom'
+import Header from './components/nav/Header'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import Home from './pages/Home'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import RegisterComplete from './pages/auth/RegisterComplete'
+import { auth } from './firebase'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import ForgotPassword from './pages/auth/ForgotPassword'
+import { currentUser } from './functions/auth'
+import History from './pages/user/History'
+import UserRoute from './components/routes/UserRoute'
+import AdminRoute from './components/routes/AdminRoute'
+import Password from './pages/user/Password'
+import Wishlist from './pages/user/Wishlist'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import CreateCategory from './pages/admin/category/CreateCategory'
+import UpdateCategory from './pages/admin/category/UpdateCategory'
+import CreateSubcategory from './pages/admin/subcategory/CreateSubcategory'
+import UpdateSubcategory from './pages/admin/subcategory/UpdateSubcategory'
+import CreateProduct from './pages/admin/product/CreateProduct'
+import AllProducts from './pages/admin/product/AllProducts'
 
 function App() {
-	const dispatch = useDispatch();
+	const dispatch = useDispatch()
 	//check firebase auth state
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
 			if (user) {
-				const idTokenResult = await user.getIdTokenResult();
+				const idTokenResult = await user.getIdTokenResult()
 				currentUser(idTokenResult.token)
 					.then((res) => {
 						dispatch({
@@ -43,14 +44,14 @@ function App() {
 								role: res.data.role,
 								_id: res.data._id,
 							},
-						});
+						})
 					})
-					.catch((err) => console.log(err));
+					.catch((err) => console.log(err))
 			}
-		});
+		})
 		//cleanup
-		return () => unsubscribe();
-	}, []);
+		return () => unsubscribe()
+	}, [])
 	return (
 		<>
 			<Header />
@@ -64,8 +65,14 @@ function App() {
 				<UserRoute path='/user/history' exact component={History}></UserRoute>
 				<UserRoute path='/user/password' exact component={Password}></UserRoute>
 				<UserRoute path='/user/wishlist' exact component={Wishlist}></UserRoute>
-				<AdminRoute path='/admin/dashboard' exact component={AdminDashboard}></AdminRoute>
-				<AdminRoute path='/admin/category' exact component={CreateCategory}></AdminRoute>
+				<AdminRoute
+					path='/admin/dashboard'
+					exact
+					component={AdminDashboard}></AdminRoute>
+				<AdminRoute
+					path='/admin/category'
+					exact
+					component={CreateCategory}></AdminRoute>
 				<AdminRoute
 					path='/admin/category/:slug'
 					exact
@@ -78,10 +85,17 @@ function App() {
 					path='/admin/subcategory/:slug'
 					exact
 					component={UpdateSubcategory}></AdminRoute>
-				<AdminRoute path='/admin/product' exact component={CreateProduct}></AdminRoute>
+				<AdminRoute
+					path='/admin/product'
+					exact
+					component={CreateProduct}></AdminRoute>
+				<AdminRoute
+					path='/admin/products'
+					exact
+					component={AllProducts}></AdminRoute>
 			</Switch>
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
