14680b2f33fc35d9c4bc6f954d797903a1c05215
shouravrahman <shouravrahman006@gmail.com>
2022-01-18 22:44:07 +0600

    cart routes controllers

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,39 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/user', (req, res) => {
-	res.json({
-		data: 'hello motherfucker you just hit the user route',
-	});
-});
+// middlewares
+const { authCheck } = require('../middlewares/auth')
+// controllers
+const {
+	userCart,
+	getUserCart,
+	emptyCart,
+	saveAddress,
+	//   applyCouponToUserCart,
+	//   createOrder,
+	//   orders,
+	//   addToWishlist,
+	//   wishlist,
+	//   removeFromWishlist,
+	//   createCashOrder,
+} = require('../controllers/user')
 
-module.exports = router;
+router.post('/user/cart', authCheck, userCart) // save cart
+router.get('/user/cart', authCheck, getUserCart) // get cart
+router.delete('/user/cart', authCheck, emptyCart) // empty cart
+router.post('/user/address', authCheck, saveAddress)
+
+// router.post('/user/order', authCheck, createOrder) // stripe
+// router.post('/user/cash-order', authCheck, createCashOrder) // cod
+// router.get('/user/orders', authCheck, orders)
+
+// // coupon
+// router.post('/user/cart/coupon', authCheck, applyCouponToUserCart)
+
+// // wishlist
+// router.post('/user/wishlist', authCheck, addToWishlist)
+// router.get('/user/wishlist', authCheck, wishlist)
+// router.put('/user/wishlist/:productId', authCheck, removeFromWishlist)
+
+module.exports = router
