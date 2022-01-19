5bb554e8bc4185d0824f797226ef86ab7b320a7b
shouravrahman <shouravrahman006@gmail.com>
2022-01-19 18:21:02 +0600

    order create,coupon apply

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const Product = require('../models/product')
 const Cart = require('../models/cart')
+const Order = require('../models/order')
 
 exports.userCart = async (req, res) => {
 	// console.log(req.body); // {cart: []}
@@ -72,6 +73,62 @@ exports.saveAddress = async (req, res) => {
 		{ email: req.user.email },
 		{ address: req.body.address }
 	).exec()
+
+	res.json({ ok: true })
+}
+exports.applyCouponToUserCart = async (req, res) => {
+	const { coupon } = req.body
+	console.log('COUPON', coupon)
+
+	const validCoupon = await Coupon.findOne({ name: coupon }).exec()
+	if (validCoupon === null) {
+		return res.json({
+			err: 'Invalid coupon',
+		})
+	}
+	console.log('VALID COUPON', validCoupon)
+
+	const user = await User.findOne({ email: req.user.email }).exec()
+
+	let { products, cartTotal } = await Cart.findOne({ orderdBy: user._id })
+		.populate('products.product', '_id title price')
+		.exec()
+
+	console.log('cartTotal', cartTotal, 'discount%', validCoupon.discount)
+
+	// calculate the total after discount
+	let totalAfterDiscount = (
+		cartTotal -
+		(cartTotal * validCoupon.discount) / 100
+	).toFixed(2) // 99.99
+
+	console.log('----------> ', totalAfterDiscount)
+
+	Cart.findOneAndUpdate(
+		{ orderdBy: user._id },
+		{ totalAfterDiscount },
+		{ new: true }
+	).exec()
+
+	res.json(totalAfterDiscount)
+}
+
+expors.createOrder = async (req, res) => {
+	//get intent
+	const { paymentIntent } = req.body.stripeResponse
+	//get user
+	const user = await User.findOne({ email: req.user.email }).exec()
+
+	//get users cart
+	let { products } = await Cart.findOne({ orderdBy: user._id }).exec()
+
+	let newOrder = await new Order({
+		products,
+		paymentIntent,
+		orderdBy: user._id,
+	}).save()
+
+	// decrement quantity,increment sold
 
 	res.json({ ok: true })
 }
