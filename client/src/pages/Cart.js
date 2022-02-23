9360cf5450d9d3b7c1010914c6ede50a3593f255
shouravrahman <shouravrahman006@gmail.com>
2022-02-23 17:08:29 +0600

    cod

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,89 +5,104 @@ import ProductCardInCheckout from '../components/cards/ProductCardInCheckout'
 import { userCart } from '../functions/user'
 
 const Cart = ({ history }) => {
-	//redux state
 	const { cart, user } = useSelector((state) => ({ ...state }))
 	const dispatch = useDispatch()
 
-	//get total price of all items
 	const getTotal = () => {
-		return cart.reduce((current, next) => {
-			return current + next.count * next.price
+		return cart.reduce((currentValue, nextValue) => {
+			return currentValue + nextValue.count * nextValue.price
 		}, 0)
 	}
-	//save order to db if user clicks checkout because of security concern.localstorage can be manipulated by the client
+
 	const saveOrderToDb = () => {
-		//
+		// console.log("cart", JSON.stringify(cart, null, 4));
 		userCart(cart, user.token)
 			.then((res) => {
-				console.log(res)
+				console.log('CART POST RES', res)
 				if (res.data.ok) history.push('/checkout')
 			})
-			.catch((err) => console.log(err))
+			.catch((err) => console.log('cart save err', err))
 	}
 
-	//show cart items
-	const showCartItems = () => {
-		return (
-			<table className='table table-bordered'>
-				<thead className='thead-light'>
-					<tr>
-						<th scope='col'>Image</th>
-						<th scope='col'>Title</th>
-						<th scope='col'>Price</th>
-						<th scope='col'>Brand</th>
-						<th scope='col'>Color</th>
-						<th scope='col'>Count</th>
-						<th scope='col'>Shipping</th>
-						<th scope='col'>Remove</th>
-					</tr>
-				</thead>
-
-				{cart.map((p) => (
-					<ProductCardInCheckout key={p._id} p={p} />
-				))}
-			</table>
-		)
+	const saveCashOrderToDb = () => {
+		// console.log("cart", JSON.stringify(cart, null, 4));
+		dispatch({
+			type: 'COD',
+			payload: true,
+		})
+		userCart(cart, user.token)
+			.then((res) => {
+				console.log('CART POST RES', res)
+				if (res.data.ok) history.push('/checkout')
+			})
+			.catch((err) => console.log('cart save err', err))
 	}
+
+	const showCartItems = () => (
+		<table className='table table-bordered'>
+			<thead className='thead-light'>
+				<tr>
+					<th scope='col'>Image</th>
+					<th scope='col'>Title</th>
+					<th scope='col'>Price</th>
+					<th scope='col'>Brand</th>
+					<th scope='col'>Color</th>
+					<th scope='col'>Count</th>
+					<th scope='col'>Shipping</th>
+					<th scope='col'>Remove</th>
+				</tr>
+			</thead>
+
+			{cart.map((p) => (
+				<ProductCardInCheckout key={p._id} p={p} />
+			))}
+		</table>
+	)
 
 	return (
 		<div className='container-fluid pt-2'>
 			<div className='row'>
-				<h4>cart / {Cart.length} Product</h4>
-			</div>
-			<div className='row'>
 				<div className='col-md-8'>
+					<h4>Cart / {cart.length} Product</h4>
+
 					{!cart.length ? (
 						<p>
-							No products in the cart. <Link to='/'>continue shopping</Link>
+							No products in cart. <Link to='/shop'>Continue Shopping.</Link>
 						</p>
 					) : (
 						showCartItems()
 					)}
 				</div>
 				<div className='col-md-4'>
-					<h4>order summary</h4>
+					<h4>Order Summary</h4>
 					<hr />
 					<p>Products</p>
-					{cart.map((c, i) => {
-						return (
-							<div key={i}>
-								<p>
-									{c.title} x {c.count} = ${c.price * c.count}
-								</p>
-							</div>
-						)
-					})}
+					{cart.map((c, i) => (
+						<div key={i}>
+							<p>
+								{c.title} x {c.count} = ${c.price * c.count}
+							</p>
+						</div>
+					))}
 					<hr />
 					Total: <b>${getTotal()}</b>
 					<hr />
 					{user ? (
-						<button
-							onClick={saveOrderToDb}
-							disabled={!cart.length}
-							className='btn btn-sm btn-primary mt-2'>
-							Proceed to checkout
-						</button>
+						<>
+							<button
+								onClick={saveOrderToDb}
+								className='btn btn-sm btn-primary mt-2'
+								disabled={!cart.length}>
+								Proceed to Checkout
+							</button>
+							<br />
+							<button
+								onClick={saveCashOrderToDb}
+								className='btn btn-sm btn-warning mt-2'
+								disabled={!cart.length}>
+								Pay Cash on Delivery
+							</button>
+						</>
 					) : (
 						<button className='btn btn-sm btn-primary mt-2'>
 							<Link
@@ -95,7 +110,7 @@ const Cart = ({ history }) => {
 									pathname: '/login',
 									state: { from: 'cart' },
 								}}>
-								Login to checkout
+								Login to Checkout
 							</Link>
 						</button>
 					)}
