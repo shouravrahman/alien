881fa0b7443c76c8148c08cc9166a8620332c25b
shouravrahman <shouravrahman006@gmail.com>
2022-05-31 01:02:31 +0600

    carousel style removed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,29 @@ import NewArrivals from '../components/home/NewArrivals'
 import BestSellers from '../components/home/BestSellers'
 import CategoryList from '../components/category/CategoryList'
 import SubcategoryList from '../components/subcategory/SubcategoryList'
+import { Carousel } from 'antd'
+
+const contentStyle = {
+	height: '400px',
+	width: '100%',
+	objectFit: 'cover',
+}
 const Home = () => {
 	return (
 		<>
-			<div className='jumbotron'>
+			<div className='mb-2'>
 				{/* slider or something */}
-				<h1>Yo</h1>
+				<Carousel effect='fade' autoplay>
+					<div>
+						<img style={contentStyle} src='/static/bundle1.webp' alt='' />
+					</div>
+					<div>
+						<img style={contentStyle} src='/static/bundle2.webp' alt='' />
+					</div>
+					<div>
+						<img style={contentStyle} src='/static/headphone1.webp' alt='' />
+					</div>
+				</Carousel>
 			</div>
 			<h4 className='text-center p-3 mt-5 mb-5 display-4 jumbotron'>New Arrivals</h4>
 			<NewArrivals />
